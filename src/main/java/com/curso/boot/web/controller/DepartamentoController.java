73ab5b560e345e161ab4292fdb0b782ee63736a5
scherida rayane <scheridarayane@gmail.com>
2021-09-16 09:32:44 -0300

    Listando departamentos

--- src/main/java/com/curso/boot/web/controller/DepartamentoController.java
+++ src/main/java/com/curso/boot/web/controller/DepartamentoController.java
@@ -1,20 +1,33 @@
 package com.curso.boot.web.controller;
 
+import com.curso.boot.domain.Departamento;
+import com.curso.boot.service.DepartamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/departamentos")
 public class DepartamentoController {
 
+    @Autowired
+    private DepartamentoService departamentoService;
+
     @GetMapping("/cadastrar")
-    public String cadastro(){
+    public String cadastrar(Departamento departamento){
         return "/departamento/cadastro";
     }
 
     @GetMapping("/listar")
     public String listar(){
         return "/departamento/lista";
+    }
+
+    @PostMapping("/salvar")
+    public String salvar(Departamento departamento){
+        departamentoService.salvar(departamento);
+        return "redirect:/departamentos/cadastrar";
     }
 }
