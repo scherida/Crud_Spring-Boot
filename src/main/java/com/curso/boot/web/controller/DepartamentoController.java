433a6779a0b5c1273a75fb1dbdf7270ce70ff61d
scherida rayane <scheridarayane@gmail.com>
2021-09-16 10:42:02 -0300

    Editando departamentos

--- src/main/java/com/curso/boot/web/controller/DepartamentoController.java
+++ src/main/java/com/curso/boot/web/controller/DepartamentoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -30,6 +31,18 @@ public class DepartamentoController {
     @PostMapping("/salvar")
     public String salvar(Departamento departamento){
         departamentoService.salvar(departamento);
+        return "redirect:/departamentos/cadastrar";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String preEditar(@PathVariable("id") Long id, ModelMap model){
+        model.addAttribute("departamento", departamentoService.buscarPorId(id));
+        return "/departamento/cadastro";
+    }
+
+    @PostMapping("/editar")
+    public String editar(Departamento departamento){
+        departamentoService.editar(departamento);
         return "redirect:/departamentos/cadastrar";
     }
 }
